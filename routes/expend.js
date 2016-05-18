a80481228864b8c2ac11067592a4f929894d7678
lie Cloud <lie Cloud>
2016-05-18 20:19:51 +0800

    edit expend

--- routes/expend.js
+++ routes/expend.js
@@ -11,10 +11,10 @@ var render = require('../lib/render');
 var posts = wrap(db.get('posts'));
 
 module.exports = function(app, route) {
-  // get request
-  app.use(route.get('/', function *() {
-    var books = yield Book.find({});
-    this.body = yield render('book/books.html', {'books': books});
-  }));
+	// get request
+	app.use(route.get('/', function*() {
+		var postList = yield posts.find({});
+		this.body = ""; //yield render('book/books.html', {'books': books});
+	}));
 
 }
